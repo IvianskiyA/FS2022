eafa591fa137aa8a47e60612f35a06839d75ac03
IvianskiyA <IvianskiyA@mail.ru>
2022-04-17 17:55:38 +0300

    1.8

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,5 +1,34 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const {good, neutral, bad} = props
+  if ((good + neutral + bad) > 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good - {good} </p>
+        <p>neutral - {neutral} </p>
+        <p>bad - {bad} </p>
+        <p>all - {good + neutral + bad} </p> 
+        <p>avarage- {(good * 1 + bad * -1) / (good + neutral + bad) } </p>
+        <p>positive - {good / (good + neutral + bad) * 100}%</p>
+      </>
+    )
+    }
+  else {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good - {good} </p>
+        <p>neutral - {neutral} </p>
+        <p>bad - {bad} </p>
+      </>
+    )
+  }
+  
+  }
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,27 +41,13 @@ const App = () => {
     </button>
   )
 
-  const DisplayNaN = ({prefix, expression, suffix}) => {
-    if (expression !== expression) {
-      return <p>{prefix} - </p>
-    }
-    else {return <p>{prefix} - {expression}{suffix} </p>}
-
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h2>statistics</h2>
-      <p>good - {good} </p>
-      <p>neutral - {neutral} </p>
-      <p>bad - {bad} </p>
-      <DisplayNaN prefix = 'all' expression = {good + neutral + bad} />
-      <DisplayNaN prefix = 'avarage' expression={(good * 1 + bad * -1) / (good + neutral + bad) } />
-      <DisplayNaN prefix = 'positive' expression={good / (good + neutral + bad) * 100} suffix = '%' />
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
